6c9b8c29d2db1be840268c11cc92d2410b532d3c
Faaiz Mir <faaizmir123@gmail.com>
2024-08-06 15:12:38 -0700

    xf

--- src/Components/Component1.jsx
+++ src/Components/Component1.jsx
@@ -1,43 +1,18 @@
 import './component1.css'; // Import the custom CSS
 
 const Component1 = () => {
-//   const images = [
-//     '/images/img1.jpg',
-//     '/images/img2.jpg',
-//     '/images/img3.jpg',
-//     '/images/img4.jpg',
-//   ];
-
-//   return (
-//     <div className="flex justify-center space-x-4">
-//       {/* Empty div with a separate image */}
-//       <div className="w-1/4 rounded-lg shadow-lg">
-//         <img src="/images/img1.jpg" alt="Separate Image" className="w-full h-64 object-cover rounded-lg" />
-//       </div>
-
-//       {/* Wrapper div for the images */}
-//       <div className="image-wrapper">
-//         {images.map((image, index) => (
-//           <div key={index} className="image-container rounded-lg shadow-lg">
-//             <img src={image} alt={`Image ${index + 1}`} className="w-full h-64 object-cover rounded-lg" />
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-
 
 return(
   <div className="flex h-screen mt-14 ">
-      <div className="w-1/2 p-4 ml-20">
-   <h1 className='mt-20  font-bold text-7xl text-red-500'>Your Partner in Content Production</h1>
-   <h2 className='text-xl mt-4'>We offer services in design, thumbnails, video production, animations, youtube channels, channel monitization.</h2>
-   <button className="bg-red-500 px-8 py-4 mt-8 font-bold hover:scale-110 transition-transform duration-300 text-white">Contact Us</button>
-   </div>
-      <div className="w-1/2 sm:mt-18">
-        <img src='/images/vector.jpg' />
-      </div>
-    </div>
+  <div className="w-1/2 p-4 ml-20">
+    <h1 className='mt-20  font-bold text-7xl text-red-500'>Your Partner in Content Production</h1>
+    <h2 className='text-xl mt-4'>We offer services in design, thumbnails, video production, animations, youtube channels, channel monitization.</h2>
+    <button className="bg-red-500 px-8 py-4 mt-8 font-bold hover:scale-110 transition-transform duration-300 text-white">Contact Us</button>
+  </div>
+  <div className="w-1/2 sm:mt-18">
+    <img src='/images/vector.jpg' />
+  </div>
+</div>
 )
  };
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,11 +11,11 @@ const Navbar = ({ handleScroll, refs }) => {
 
   return (
     <div className="fixed top-0 left-0 right-0 z-50 bg-gray-200">
-    <nav className="p-4 flex justify-between items-center h-24">  {/* Set a specific height */}
+    <nav className="p-6 py-7 flex justify-between items-center h-24">  {/* Set a specific height */}
       <div className="text-white text-xl font-bold">
-        <img className="object-contain h-32" src="images/visualscreationlogo.png" alt="Your Logo" />  {/* Adjust logo height */}
+        <img className="object-contain h-28" src="images/visualscreationlogo.png" alt="Your Logo" />  {/* Adjust logo height */}
       </div>
-      <div className="hidden md:flex space-x-4">
+      <div className="hidden md:flex space-x-14 mr-28">
         <Link to="/" className=" text-red-800 text-2xl hover:text-red hover:text-3xl ">About</Link>
         <button className=" text-red-800 text-2xl hover:text-red hover:text-3xl " onClick={() => handleScroll(refs.refA)}>Services</button>
         <button className=" text-red-800 text-2xl hover:text-red hover:text-3xl " onClick={() => handleScroll(refs.refB)}>Past Projects</button>
@@ -48,11 +48,6 @@ const Navbar = ({ handleScroll, refs }) => {
 export default Navbar;
 
 
-// //  <div className="hidden md:flex space-x-4">
-// <Link to="/" className="text-white text-xl hover:text-gray-300 hover:text-2xl">About</Link>
-// <button className="text-white text-xl hover:text-gray-300 hover:text-2xl" onClick={() => handleScroll(refs.refA)}>Services</button>
-// <button className="text-white text-xl hover:text-gray-300 hover:text-2xl" onClick={() => handleScroll(refs.refB)}>Past Projects</button>
-// </div>
 
 
 
@@ -62,34 +57,3 @@ export default Navbar;
 
 
 
-// <div className="fixed top-0 left-0 right-0 z-50 bg-gray-200">
-//     <nav className="p-6 flex justify-between items-center h-40">
-//       <div className="text-white text-xl font-bold">
-//         <img className="object-contain h-60" src="images/visualcreationslogo.png" alt="Your Logo" />
-//       </div>
-//       <div className="hidden md:flex space-x-4">
-//         <Link to="/" className=" text-red-800 text-2xl hover:text-red hover:text-3xl font-bold">About</Link>
-//         <button className=" text-red-800 text-2xl hover:text-red hover:text-3xl font-bold" onClick={() => handleScroll(refs.refA)}>Services</button>
-//         <button className=" text-red-800 text-2xl hover:text-red hover:text-3xl font-bold" onClick={() => handleScroll(refs.refB)}>Past Projects</button>
-//         <button className=" text-red-800 text-2xl hover:text-red hover:text-3xl font-bold">Testimonials</button>
-//         <button className=" text-red-800 text-2xl hover:text-red hover:text-3xl font-bold">Clients</button>
-//         <button className=" text-red-800 text-2xl hover:text-red hover:text-3xl font-bold">Contact</button>
-//       </div>
-//       <div className="md:hidden">
-//         <button onClick={toggleMenu} className="text-red-800">
-//         <img src='/images/hamburger.png' />
-//         </button>
-//       </div>
-//     </nav>
-//     <div className={`fixed top-0 right-0 w-60 mt-16 bg-gray-200 z-40 ${isOpen ? 'block' : 'hidden'}`}>
-//   <div className="flex flex-col h-full justify-center">
-//     <button className="text-red-800 text-xl mb-4">About</button>
-//     <button className="text-red-800 text-xl mb-4">Services</button>
-//     <button className="text-red-800 text-xl mb-4">Past Projects</button>
-//     <button className="text-red-800 text-xl mb-4">Testimonials</button>
-//     <button className="text-red-800 text-xl mb-4">Clients</button>
-//     <button className="text-red-800 text-xl mb-4">Contact</button>
-//   </div>
-// </div>
-
-//   </div>
